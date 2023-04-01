75a7bb5f999fd5925c4ddf35ad35344c708b552d
cloer <chltjrrb97@gmail.com>
2023-04-01 20:55:41 +0900

    ADD: tailwindcss primary, rontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.emerald,
+      },
+    },
   },
   plugins: [],
 };
