08626f44c442623229337c0a8369122e4bd27cd8
cloer <chltjrrb97@gmail.com>
2023-04-03 23:03:01 +0900

    UPDATE: draw Stations on Map when selectedPoint is changed

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,15 +1,21 @@
+import type { StationInfo } from '@/types/entity.type';
 import { Map } from 'react-kakao-maps-sdk';
+import { useEffect, useState } from 'react';
 import { UOS_POSITION } from '@/configs/defaultValues';
 import useWatchPosition from '@/hooks/useWatchPosition';
-import CurrentLocationMarker from '@/components/CurrentLocationMarker';
 import PanToCurrentLocationButton from '@/components/PanToCurrentLocationButton';
+import CurrentLocationMarker from '@/components/CurrentLocationMarker';
 import { useSelectedPoint } from '@/contexts/SelectedPointContext';
 import SelectedPointMarker from '@/components/SelectedPointMarker';
+import StationMarker from '@/components/StationMarker';
 import PathPanel from '@/components/pathPanel/PathPanel';
+import fetchStationsNearby from '@/fetches/server/fetchStationsNearby';
+import fetchStationStatus from '@/fetches/thirdParty/fetchStationStatus';
 
 export default function MainMap() {
   const currentPosition = useWatchPosition();
-  const { setSelectedPoint } = useSelectedPoint();
+  const { selectedPoint, setSelectedPoint } = useSelectedPoint();
+  const [stations, setStations] = useState<StationInfo[]>([]);
 
   const onMapClick = (target: kakao.maps.Map, mouseEvent: kakao.maps.event.MouseEvent) => {
     (document.activeElement as HTMLElement).blur(); // blur input
@@ -18,6 +24,31 @@ export default function MainMap() {
       lng: mouseEvent.latLng.getLng(),
     });
   };
+
+  // TODO: fetch stations near by selected point
+  useEffect(() => {
+    if (selectedPoint === null) return;
+    const getStationsNearBySelectedPoint = async () => {
+      const stationNearbyResult = await fetchStationsNearby(selectedPoint);
+      if (!stationNearbyResult.success) return console.log(stationNearbyResult); // TODO: error handling
+
+      const nearStations = stationNearbyResult.data;
+
+      // FIXME: dummy data -> seoul bike api data
+      const dummyStations: StationInfo[] = nearStations.map(({ lat, lng, stationId, address, stationName }) => ({
+        id: stationId,
+        lat,
+        lng,
+        address,
+        name: stationName,
+        rackCount: 10,
+        availableBikeCount: 5,
+      }));
+
+      setStations(dummyStations);
+    };
+    getStationsNearBySelectedPoint();
+  }, [selectedPoint]);
 
   return (
     <Map
@@ -31,6 +62,10 @@ export default function MainMap() {
           <PanToCurrentLocationButton to={currentPosition.coords} />
         </>
       )}
+
+      {stations.map((stationInfo) => (
+        <StationMarker key={stationInfo.id} station={stationInfo} />
+      ))}
 
       <SelectedPointMarker />
       <PathPanel />
