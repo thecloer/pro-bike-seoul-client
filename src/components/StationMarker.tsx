be71acae8bd8b03e6e5505dc28a2039e2beeab8e
cloer <chltjrrb97@gmail.com>
2023-04-03 23:00:58 +0900

    ADD: StationMarker. onMouseDown for not trigger MainMap onClick, memo for rerender when new selectedPoint is close

---
+++ src/components/StationMarker.tsx
@@ -0,0 +1,33 @@
+import type { StationInfo } from '@/types/entity.type';
+import React from 'react';
+import CustomDiv from '@/components/CustomDiv';
+import { ReactComponent as LocationIcon } from '@/lib/svg/location.svg';
+import { useSelectedPoint } from '@/contexts/SelectedPointContext';
+import usePanTo from '@/hooks/usePanTo';
+
+type Props = {
+  station: StationInfo;
+};
+
+export default React.memo(function StationMarker({
+  station: { id: stationId, name: stationName, lat, lng, address, rackCount, availableBikeCount },
+}: Props) {
+  const { setSelectedPoint } = useSelectedPoint();
+  const panTo = usePanTo();
+  const onMarkerClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    setSelectedPoint(null);
+    panTo({ lat, lng });
+  };
+
+  // TODO: info window
+  return (
+    <CustomDiv
+      center={{ lat, lng }}
+      className='relative hover:cursor-pointer h-10 w-10'
+      onClick={onMarkerClick}
+      onMouseDown={(e) => e.stopPropagation()}
+    >
+      <LocationIcon className='absolute bottom-1/2 left-0 w-full h-full p-1 text-primary-600' />
+    </CustomDiv>
+  );
+});
