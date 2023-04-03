739c8618fb66a71d6b6a95468ddbc04c32009b6f
cloer <chltjrrb97@gmail.com>
2023-04-03 23:11:25 +0900

    UPDATE: CurrentLocationMarker improve absolute position accurancy

--- src/components/CurrentLocationMarker.tsx
+++ src/components/CurrentLocationMarker.tsx
@@ -7,8 +7,8 @@ type Props = {
 export default function CurrentLocationMarker({ center }: Props) {
   return (
     <CustomDiv center={center} className='relative w-3 h-3'>
-      <div className='absolute top-1/2 left-0 w-full h-full rounded-full bg-sky-500/80' />
-      <div className='absolute top-1/2 left-0 w-full h-full rounded-full animate-ping bg-sky-500'></div>
+      <div className='absolute top-0 left-0 w-full h-full rounded-full bg-sky-500/80' />
+      <div className='absolute top-0 left-0 w-full h-full rounded-full animate-ping bg-sky-500'></div>
     </CustomDiv>
   );
 }
