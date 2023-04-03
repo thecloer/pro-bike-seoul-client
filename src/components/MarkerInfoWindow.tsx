9812fb37d4895e59e8a0ae6c56eb66f9bbc4a186
cloer <chltjrrb97@gmail.com>
2023-04-04 02:16:00 +0900

    UPDATE: MarkerInfoWindow expand hover area

--- src/components/MarkerInfoWindow.tsx
+++ src/components/MarkerInfoWindow.tsx
@@ -4,7 +4,9 @@ export default function MarkerInfoWindow({ children }: PropsWithChildren) {
   return (
     <div className='absolute -top-2/3 left-1/2 -translate-x-1/2 -translate-y-full flex flex-col bg-white py-2 px-3 rounded-lg shadow border-2 border-primary-600 text-sm text-slate-800'>
       {children}
-      <div className='absolute top-full left-1/2 -translate-x-1/2 border-8 border-transparent border-t-primary-600' />
+      <div className='absolute top-full left-1/2 -translate-x-1/2 px-7 pb-5'>
+        <div className='border-8 border-transparent border-t-primary-600' />
+      </div>
     </div>
   );
 }
