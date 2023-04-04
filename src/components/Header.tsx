31e0bed1738b60203611a4c867a317a46d02de84
cloer <chltjrrb97@gmail.com>
2023-04-04 16:21:33 +0900

    ADD: bike icons on header and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ function Header() {
   return (
     <header className='border-b-2'>
       <div className='container mx-auto px-4 h-16 flex items-center justify-between'>
-        <div className='text-xl font-semibold'>프로 따릉러</div>
+        <div className='flex gap-2 items-center'>
+          <img src='/bike-logo-500.png' alt='bike logo' className='w-8 h-8' />
+          <span className='text-xl font-semibold'>프로 따릉러</span>
+        </div>
         <div>
           <a href={siteMeta.githubUrl} className='text-primary-400 transition-colors hover:text-primary-500'>
             <span className='sr-only'>github link</span>
