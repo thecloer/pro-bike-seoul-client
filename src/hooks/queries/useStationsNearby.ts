7a68d21a5c8422b201780e44e463f64995b712d0
cloer <chltjrrb97@gmail.com>
2023-04-07 13:48:41 +0900

    UPDATE: useStationsNearby caches only keyword search results. not clicked points

--- src/hooks/queries/useStationsNearby.ts
+++ src/hooks/queries/useStationsNearby.ts
@@ -1,19 +1,19 @@
-import type { Position } from '@/types/geo.type';
+import type { SelectedPoint } from '@/types/data.type';
 import { useQuery } from '@tanstack/react-query';
 import fetchStationsNearby from '@/fetches/server/fetchStationsNearby';
 
-export default function useStationsNearby(point: Position | null) {
+export default function useStationsNearby(selectedPoint: SelectedPoint) {
   return useQuery({
-    queryKey: ['stationNearby', point],
-    queryFn: () => point && fetchStationsNearby(point),
-    enabled: !!point,
+    queryKey: ['stationNearby', selectedPoint],
+    queryFn: () => selectedPoint && fetchStationsNearby(selectedPoint),
+    enabled: !!selectedPoint,
     select: (response) => {
       if (response?.success) return response.data;
       console.log('ERROR in stationsNearbyQuery');
       console.error(response);
       return [];
     },
-    cacheTime: point ? 5 * 60 * 1000 : Infinity, // 좌표 별 주변 대여소 검색결과 캐시
+    cacheTime: selectedPoint?.text ? 5 * 60 * 1000 : 0, // 검색결과 주변 대여소 검색결과 캐시
     staleTime: Infinity,
   });
 }
