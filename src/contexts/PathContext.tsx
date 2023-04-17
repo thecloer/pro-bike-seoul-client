cbf8f01d076aad00179a77ae6994ecc161bbe6d8
cloer <chltjrrb97@gmail.com>
2023-04-18 02:17:30 +0900

    UPDATE: PathContext add points, waypoints

--- src/contexts/PathContext.tsx
+++ src/contexts/PathContext.tsx
@@ -1,32 +1,43 @@
-import type { PathPointInfo } from '@/types/data.type';
+import type { PathPointInfo, Position } from '@/types/data.type';
 import { useReducer, createContext, PropsWithChildren, useContext } from 'react';
 
 type PathContextState = {
   start: PathPointInfo;
   end: PathPointInfo;
+  points: Position[] | null;
+  waypoints: Position[] | null;
 };
 
 const defaultPath: PathContextState = {
   start: { text: '' },
   end: { text: '' },
+  points: null,
+  waypoints: null,
 };
 
 type PathContextAction =
   | { type: 'SWAP_POINTS' }
   | { type: 'SET_START'; point: PathPointInfo }
-  | { type: 'SET_END'; point: PathPointInfo };
+  | { type: 'SET_END'; point: PathPointInfo }
+  | { type: 'SET_PATH'; points: Position[]; waypoints: Position[] }
+  | { type: 'DELETE_PATH' };
 
 function reducer(state: PathContextState, action: PathContextAction): PathContextState {
-  const { start, end } = state;
   switch (action.type) {
     case 'SWAP_POINTS': {
-      return { start: end, end: start };
+      return { ...state, start: state.end, end: state.start };
     }
     case 'SET_START': {
       return { ...state, start: action.point };
     }
     case 'SET_END': {
       return { ...state, end: action.point };
+    }
+    case 'SET_PATH': {
+      return { ...state, points: action.points, waypoints: action.waypoints };
+    }
+    case 'DELETE_PATH': {
+      return { ...state, points: null, waypoints: null };
     }
   }
 }
@@ -36,6 +47,7 @@ type PathContext = {
   swapDestination: () => void;
   setStartPoint: (point: PathPointInfo) => void;
   setEndPoint: (point: PathPointInfo) => void;
+  setPath: (path: { points: Position[]; waypoints: Position[] } | null) => void;
 };
 
 const PathContext = createContext<PathContext>({
@@ -43,6 +55,7 @@ const PathContext = createContext<PathContext>({
   swapDestination: () => {},
   setStartPoint: () => {},
   setEndPoint: () => {},
+  setPath: () => {},
 });
 
 export const usePath = () => useContext(PathContext);
@@ -53,6 +66,10 @@ export function PathProvider({ children }: PropsWithChildren) {
   const swapDestination = () => dispatch({ type: 'SWAP_POINTS' });
   const setStartPoint = (point: PathPointInfo) => dispatch({ type: 'SET_START', point });
   const setEndPoint = (point: PathPointInfo) => dispatch({ type: 'SET_END', point });
+  const setPath = (path: { points: Position[]; waypoints: Position[] } | null) => {
+    if (path === null) dispatch({ type: 'DELETE_PATH' });
+    else dispatch({ type: 'SET_PATH', ...path });
+  };
 
   return (
     <PathContext.Provider
@@ -61,6 +78,7 @@ export function PathProvider({ children }: PropsWithChildren) {
         swapDestination,
         setStartPoint,
         setEndPoint,
+        setPath,
       }}
     >
       {children}
