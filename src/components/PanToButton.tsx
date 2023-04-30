1a9b7e1e587b920ff54176eacdb2ee5d295aa4ef
cloer <chltjrrb97@gmail.com>
2023-04-30 21:00:09 +0900

    UPDATE: PanToButton sets Level as well

--- src/components/PanToButton.tsx
+++ src/components/PanToButton.tsx
@@ -1,5 +1,6 @@
 import type { Position } from '@/types/data.type';
 import type { PropsWithChildren } from 'react';
+import { useMap } from 'react-kakao-maps-sdk';
 import usePanTo from '@/hooks/usePanTo';
 
 type Props = {
@@ -8,9 +9,13 @@ type Props = {
 };
 
 export default function PanToButton({ to, className, children }: PropsWithChildren<Props>) {
+  const Map = useMap();
   const panTo = usePanTo();
 
-  const onClick = () => panTo(to);
+  const onClick = () => {
+    panTo(to);
+    Map.setLevel(4);
+  };
 
   return (
     <button className={className} onClick={onClick}>
