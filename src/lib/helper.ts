dafa07f5d487399c8cc3c4a31ac0c3c0b68fc780
cloer <chltjrrb97@gmail.com>
2023-05-08 18:39:37 +0900

    UPDATE: set dom hight

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,5 +1,10 @@
 import { Position } from '@/types/data.type';
 
+export const setDomHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
 export const isPosition = <T extends Position>(point: any): point is T => 'lat' in point && 'lng' in point;
 export const positionFilter = <T extends Position>(point: T): Position => ({
   lat: point.lat,
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import App from '@/App';
 import '@/index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { setDomHeight } from './lib/helper';
 
 const queryClient = new QueryClient({});
+
+setDomHeight();
+window.addEventListener('resize', () => setDomHeight());
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
