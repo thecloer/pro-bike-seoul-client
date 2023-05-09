62d297f2846d41932a6c6ad5f8fd2467c1a8b4fe
cloer <chltjrrb97@gmail.com>
2023-05-09 13:18:47 +0900

    UPDATE: siteMeta

--- src/configs/siteMeta.ts
+++ src/configs/siteMeta.ts
@@ -1,7 +1,7 @@
 const siteMeta = {
   title: '프로 따릉러',
   description: '잔여 따릉이 확인, 따릉이 경탐색을 한번에',
-  githubUrl: 'https://www.thecloer.com/blog/posts/projects/pro-bike-seoul', // TODO: blog url
+  githubUrl: 'https://www.thecloer.com/blog/posts/projects/pro-bike-seoul/index',
 } as const;
 
 export default siteMeta;
